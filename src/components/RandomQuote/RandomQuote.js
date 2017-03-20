a7698744be0ab35e9d5a31d8376313ad364470e5
Hugh Morrison <hmorri32@gmail.com>
2017-03-20 15:00:51 -0600

    randomquote component

---
+++ src/components/RandomQuote/RandomQuote.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const RandomQuote = () => {
+  return (
+    <p> 
+  )
+}
+
+export default RandomQuote
--- src/components/SwapiBox/SwapiBox.js
+++ src/components/SwapiBox/SwapiBox.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import RandomQuote from '../RandomQuote/RandomQuote'
 
 class SwapiBox extends Component {
   constructor() {
     super()
     this.state ={
-      
+      randomQuote: ''
     }
   }
 
@@ -15,15 +15,25 @@ class SwapiBox extends Component {
       return response.json()
     })
     .then((json) => {
-      const data = json.results[0].opening_crawl
-      console.log(data);
+      const randomResult  = Math.random(json.results) * json.count
+      const flooredResult = Math.floor(randomResult)
+      const randomQuote   = json.results[flooredResult].opening_crawl
+      this.setState({
+        randomQuote: randomQuote
+      })
     })
     console.log('did mount');
   }
 
   render() {
     return (
-      <div>{  }</div>
+      <div className='wrapper'>
+        <div className='random-quote'>
+          <RandomQuote quote={ this.state.randomQuote }/>
+          { this.state.randomQuote }
+        </div>
+
+      </div>
     )
   }
 }
