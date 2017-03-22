9c32434d6ae9eb858942b67a0190a45b739a2953
Hugh Morrison <hmorri32@gmail.com>
2017-03-22 14:18:31 -0600

    refactor card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -18,11 +18,11 @@ const Card = ({ cardInfo, specieInfo, planetInfo, shipInfo }) => {
   if(planetInfo){
     return(
       <div>
-        <h3>{ planetInfo.name }</h3>
+        <h2>{ planetInfo.name }</h2>
         <h4>Terrain: { planetInfo.terrain}</h4>
         <h4>Population: { planetInfo.population }</h4>
         <h4>Climate: { planetInfo.climate }</h4>
-        {planetInfo.residentInfo ? <h3>Resident: {planetInfo.residentInfo.name}</h3> : null}
+        {planetInfo.residentInfo ? <h4>Resident: {planetInfo.residentInfo.name}</h4> : null}
       </div>
     )
   }
@@ -30,10 +30,10 @@ const Card = ({ cardInfo, specieInfo, planetInfo, shipInfo }) => {
   if(cardInfo && specieInfo){
     return (
       <div>
-        <h3>{ cardInfo.name }</h3>
+        <h2>{ cardInfo.name }</h2>
         <h4>Species: { specieInfo.name }</h4>
-        <h4>Homeworld: { cardInfo.worldName }</h4>
         <h4>Language: { specieInfo.language } </h4>
+        <h4>Homeworld: { cardInfo.worldName }</h4>
         <h4>Population: { cardInfo.planetInfo.population } </h4>
       </div>
     )
