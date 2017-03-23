53393cee66091d9b432f3f3abe51d8d280fac6aa
Hugh Morrison <hmorri32@gmail.com>
2017-03-22 21:05:31 -0600

    person setstate tests

--- src/components/People/Person.test.js
+++ src/components/People/Person.test.js
@@ -3,6 +3,17 @@ import { shallow, mount } from 'enzyme';
 import Person             from './Person';
 
 describe('testing Person component', () => {
+
+  const mockPeople = [
+    { name: 'planet town',
+      species: 'Pineapples',
+      population: 10,
+      climate: 'hot!',
+      language: 'english',
+      homeworld: []
+    }
+  ];
+
   it('Person should have a default state homeworld that is an empty string, ', () => {
     const wrapper = shallow( <Person /> )
     const state   = wrapper.state();
@@ -17,24 +28,77 @@ describe('testing Person component', () => {
     expect(state.species).toBe('')
   })
 
-  it('Person should have a default state homeworld that is an empty string, ', () => {
+  it('Person should have a default state population that is an empty string, ', () => {
     const wrapper = shallow( <Person /> )
     const state   = wrapper.state();
 
-    expect(state.category).toBe('')
+    expect(state.population).toBe('')
   })
 
-  it('Person should have a default state homeworld that is an empty string, ', () => {
-    const wrapper = shallow( <Person /> )
+  it('Person should have a default state language that is an empty string, ', () => {
+    const wrapper = mount( <Person
+                                name='planet'
+                                homeworld='suhh'
+                                species='dude'
+                                population='alot'
+                                language='english'/>
+                            )
     const state   = wrapper.state();
-
-    expect(state.category).toBe('')
+    expect(state.language).toBe('')
   })
 
-  it('Person should have a default state homeworld that is an empty string, ', () => {
-    const wrapper = shallow( <Person /> )
-    const state   = wrapper.state();
-
-    expect(state.category).toBe('')
+  it('Person should receive props and render ', () => {
+    const wrapper = mount(  <Person
+                                name='chad'
+                                homeworld='suhh'
+                                species='dude'
+                                population='alot'
+                                language='english'/>
+                            )
+    expect(wrapper.props().name).toBe('chad')
+    expect(wrapper.props().homeworld).toBe('suhh')
+    expect(wrapper.props().species).toBe('dude')
+    expect(wrapper.props().population).toBe('alot')
+    expect(wrapper.props().language).toBe('english')
+    expect(wrapper.find('Person').length).toBe(1)
+    expect(wrapper.find('.cards-wrapper').length).toBe(1)
   })
+
+  it.only('Person should receive props, update state, and render ', () => {
+    const wrapper = mount(  <Person
+                                name='chad'
+                                homeworld='suhh'
+                                species='dude'
+                                population='alot'
+                                language='english'/>
+                            )
+    const mockPeople = {
+        name: 'planet town',
+        species: 'Pineapples',
+        population: 10,
+        climate: 'hot!',
+        language: 'english',
+        homeworld: 'naboo',
+      }
+
+    expect(wrapper.props().name).toBe('chad')
+    expect(wrapper.find('Person').length).toBe(1)
+    expect(wrapper.find('.cards-wrapper').length).toBe(1)
+
+    wrapper.setState({
+      homeworld: mockPeople.homeworld,
+      species: mockPeople.species,
+      population: mockPeople.population,
+      language: mockPeople.language
+     })
+
+    console.log(wrapper.debug());
+
+    expect(wrapper.contains(<h2>chad</h2>)).toEqual(true);
+
+    expect(wrapper.state().homeworld).toEqual('naboo')
+    expect(wrapper.state().species).toEqual('Pineapples')
+    expect(wrapper.state().population).toEqual(10)
+  })
+
 })
